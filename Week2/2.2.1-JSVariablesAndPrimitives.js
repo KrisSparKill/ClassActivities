facb1f613721082e6e29517fc4decdf8a5c64094
Christy Nicholson <christy.nicholson@gmail.com>
2023-11-17 10:56:27 -0500

    Week 2/ 2.2.1

--- Week2/2.2.1-JSVariablesAndPrimitives.js
+++ Week2/2.2.1-JSVariablesAndPrimitives.js
@@ -1,11 +1,12 @@
 // create a constant variable (const)
-Const myName = "Christy"
+// eslint-disable-next-line no-unused-vars
+const myName = "Christy";
 // create a variable that can be reassigned (let)
 let age = 39;
 // create three variables and assign them values of different data types (=)
 let language = "Spanish";
-let thing = true;
-let that = 3;
+let thing = false;
+let that = 12;
 // print the types of two variables that reference two different data types (typeof)
 // TIP: console.log() prints a value in the console/terminal
 console.log(typeof thing);
@@ -13,13 +14,16 @@ console.log(typeof that);
 // change the value referenced by a variable (dynamic typing)
 age = 44;
 // print the type of the variable you just changed (typeof)
-console.log(typeof age)
+console.log(typeof age);
 // create variables and give them values to complete the sentences that will print  below.
 // Which variables need to be created?
 // What type of data needs to go in each variable?
-const myName = "Christy";
+// eslint-disable-next-line no-unused-vars
+let name = "Christy";
 language = "Greek";
-that = 4;
-level = false;
-console.log("Hello, my name is " + myName + ", and I am learning " + language + ". I have been practicing for only " +  that + " weeks, so my status as a master is: " + level + ".");
-
+// eslint-disable-next-line prettier/prettier
+let number = 4
+// eslint-disable-next-line prettier/prettier
+let master = false
+// eslint-disable-next-line prettier/prettier
+console.log("Hello, my name is " + name + ", and I am learning " + language + ". I have been practicing for only " +  number + " weeks, so my status as a master is: " + master + ".");
