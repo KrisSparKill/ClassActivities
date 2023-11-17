7b299d56786cd9934e709bad0dc44295f04277d2
Christy Nicholson <christy.nicholson@gmail.com>
2023-11-17 10:53:04 -0500

    Week 2

---
+++ Week2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,25 @@
+// create a constant variable (const)
+Const myName = "Christy"
+// create a variable that can be reassigned (let)
+let age = 39;
+// create three variables and assign them values of different data types (=)
+let language = "Spanish";
+let thing = true;
+let that = 3;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof thing);
+console.log(typeof that);
+// change the value referenced by a variable (dynamic typing)
+age = 44;
+// print the type of the variable you just changed (typeof)
+console.log(typeof age)
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+const myName = "Christy";
+language = "Greek";
+that = 4;
+level = false;
+console.log("Hello, my name is " + myName + ", and I am learning " + language + ". I have been practicing for only " +  that + " weeks, so my status as a master is: " + level + ".");
+
