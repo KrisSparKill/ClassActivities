54bce30954fa8929ddc9f9c277965206bd0a497e
Christy Nicholson <christy.nicholson@gmail.com>
2023-11-17 19:30:08 -0500

    2.3.2 update

--- Week2/2.3.2-ComparisonOperatorsAndIfElseStatements.js
+++ Week2/2.3.2-ComparisonOperatorsAndIfElseStatements.js
@@ -6,10 +6,10 @@ let age = 25;
 // console.log "Renting a car is more affordable."
 // if age is less than 25
 // console.log "Renting a car is very expensive."
-if (age <=25) {
-  console.log(Renting a car is more affordable.);
+if (age >=25) {
+  console.log("Renting a car is more affordable.");
 } else {
-  console.log(Renting a car is very expensive.):
+  console.log("Renting a car is very expensive.");
 }
 
 // Chaining if statements: if - else if - ... - else
@@ -20,25 +20,13 @@ if (age <=25) {
 // console.log "Renting a car is very expensive."
 // if age is less than 18
 // console.log "You cannot legally rent a car."
-const age = 25;
-switch (true) {
-  case age >= 25: {
-    const message = "Renting a car is more affordable.";
-    console.log(message);
-    break;
-  }
-  case age >= 18 && age < 25: {
-    const message = "Renting a car is very expensive."
-    console.log(message);
-    break;
-  }
-  case age < 18: {
-    const message = "You cannot legally rent a car."
-    console.log(message);
-    break;
-  }
-  default: {
-    console.log("invalid age");
-  }
+if (age >= 25) {
+  console.log("Renting a car is more affordable.");
+} else if (age < 25 && >= 18) {
+  console.log("Renting a car is very expensive.");
+} else if (age < 18) {
+  console.log("You cannot legally rent a car.");
+} else {
+  console.log("Invalid age.");
 }
 
