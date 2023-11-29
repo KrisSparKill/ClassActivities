de1d29a8fa3cee04332e549c1d5967d030bfd3d3
Christy Nicholson <christy.nicholson@gmail.com>
2023-11-28 21:38:55 -0500

    Selecting DOM Elements

---
+++ Week4/4.2Activities/4.2.1-SelectingDOMElements.js
@@ -0,0 +1,16 @@
+//Showing both ways to do these.
+//select all unordered list (ul) elements
+document.querySelectorAll("ul");
+document.getElementsByTagName("ul");
+
+// select all elements with the class "class-week"
+document.querySelectorAll(".class-week");
+document.getElementsByClassName("class-week");
+
+// select all elements with the class "nav-bar"
+document.querySelectorAll(".nav-bar");
+document.getElementsByClassName("nav-bar");
+
+// select the element with the id "dog-picture" and save it to a variable
+document.querySelector("dog-picture");
+document.getElementById("dog-picture");
