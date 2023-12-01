4f57f0927325d93edfe354377f106e537c7a9264
Christy Nicholson <christy.nicholson@gmail.com>
2023-11-30 20:43:20 -0500

    Event Listeners 2

--- Week4/4.2Activities/4.2.5-EventListeners.js
+++ Week4/4.2Activities/4.2.5-EventListeners.js
@@ -1,20 +1,22 @@
 // add an event listener to the dog picture that listens for a "click" and gives an alert
-document.querySelector("#dog-picture")
-  .addEventListener("click", (event) => {
-      console.log("Dog picture has been clicked");
-  })
+document.querySelector("#dog-picture").addEventListener("click", event => {
+  console.log("Dog picture has been clicked");
+});
 
 // add an event listener to the list items in the class schedule that listens for a "mouseover" event and changes its background color
 const listItems = document.querySelectorAll("#class-schedule-list li")
 console.log(listItems);
 for (let item of listItems) {
-  item.addEventListener("mouseover", (event) => {
+  item.addEventListener("mouseover", event => {
     event.target.style.backgroundColor = "red";
   });
-  item.addEventListener("mouseout", (event) => event.target.style.backgroundColor = "inherit");
+  item.addEventListener(
+    "mouseout",
+    event => (event.target.style.backgroundColor = "inherit")
+  );
 }
 
-// add an event listener to the whole page that listens for a "keydown" event and prints the key that was pressed
+// add an event listener to the whole page that listens for a "keyup" event and prints the key that was pressed
 document.addEventListener("keyup", (event) => {
   console.log(`the ${event.key} was pressed.`);
 })
